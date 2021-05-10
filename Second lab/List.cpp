afe847018d1193e2c340b5065538f8a88b67bd47
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2021-05-10 12:44:58 +0300

    added functional for list

--- Second lab/List.cpp
+++ Second lab/List.cpp
@@ -1,5 +1,51 @@
-//
-// Created by polina on 24.03.2021.
-//
-
 #include "List.h"
+#include "iostream"
+#include <list>
+
+using namespace std;
+list<int> List::getValue() const {
+    return myList;
+}
+
+void List::setValue(list<int> List) {
+    List::myList = List;
+}
+
+
+List::~List() = default;
+
+_List_const_iterator<int> List::findElement(int x){
+        list<int>::const_iterator it;
+        it = this->myList.begin();
+    while (it != myList.end()) {
+        if(*it != x){
+            it++;
+        }
+        return it;
+    }
+}
+
+List& List::deleteElement(int x){
+    auto deleteEl = this->findElement(x);
+    this->myList.erase(deleteEl);
+    return *this;
+}
+List& List::addElement(int x){
+   this->myList.push_back(x);
+    return *this;
+}
+
+List::List(const list<int> &myList) : containers(), myList(myList) {}
+
+int containers::getNumOfElements() const {
+    return numOfElements;
+}
+void containers::setNumOfElements(int numOfElements) {
+    containers::numOfElements = numOfElements;
+}
+void List::showList(){
+    list<int>::const_iterator it;
+    for (it = this->myList.begin(); it != this->myList.end(); it++){
+        cout << *it << " ";
+    }
+}
--- Second lab/List.h
+++ Second lab/List.h
@@ -1,13 +1,32 @@
-
 #ifndef SECOND_LAB_LIST_H
 #define SECOND_LAB_LIST_H
-
-
-class List {
-
+#include "list"
+using namespace std;
+class containers{
+private:
+    int numOfElements;
+public:
+    int getNumOfElements() const;
+    void setNumOfElements(int numOfElements);
 };
-class Stack{
 
+class List : public containers {
+private:
+    list<int> myList;
+public:
+    explicit List(const list<int> &myList);
+    list<int> getValue() const;
+    void setValue(list<int> List);
+
+    _List_const_iterator<int> findElement(int x);
+    List &deleteElement(int x);
+    List &addElement(int x);
+
+    virtual ~List();
+
+    void showList();
 };
+
+
 
 #endif //SECOND_LAB_LIST_H
